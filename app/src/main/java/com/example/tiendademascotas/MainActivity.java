fb31cf81fcaad152e70656ee8cf7fc4ed4f450da
Eddo091 <44987358+eddo091@users.noreply.github.com>
2020-11-27 23:23:07 -0600

    Chats (día viernes 27/11/20)  Tercera Actualizacion despues de depurar (foto directorio, lstmascotas vista)

--- app/src/main/java/com/example/tiendademascotas/MainActivity.java
+++ app/src/main/java/com/example/tiendademascotas/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -145,10 +146,11 @@ public class MainActivity extends AppCompatActivity {
 
     private File createImageFile () throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "imagen_" + timeStamp + "_";
         File storageDir = getExternalFilesDir( Environment.DIRECTORY_PICTURES);
-        if( storageDir.exists()==false ){
+        assert storageDir != null;
+        if(!storageDir.exists()){
             storageDir.mkdirs();
         }
         File image = File.createTempFile(
--- app/src/main/java/com/example/tiendademascotas/listamascotas.java
+++ app/src/main/java/com/example/tiendademascotas/listamascotas.java
@@ -59,7 +59,7 @@ public class listamascotas extends AppCompatActivity {
 
     private void mostrarlistadoMascotas() {
 
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference( "Usuarios" );
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference( "Mascotas" );
         mDatabaseReference.orderByChild( "token" ).equalTo( myFirebaseInstanceIdService.miToken ).addListenerForSingleValueEvent( new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
